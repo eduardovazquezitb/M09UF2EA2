a68686b94a9621ca641faa1641a368bd0d362cd6
Eduardo Vázquez <eduardo.vazquez.7e5@itb.cat>
2023-02-09 17:29:10 +0100

    Add project files.

---
+++ M09UF2EA2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M09UF2EA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
