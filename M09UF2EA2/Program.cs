38f68fba4777b048a44445e026d8a7d6138fc774
Eduardo Vázquez <eduardo.vazquez.7e5@itb.cat>
2023-02-09 18:08:37 +0100

    Finished Strings Exercise

--- M09UF2EA2/Program.cs
+++ M09UF2EA2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace M09UF2EA2
 {
@@ -6,7 +8,56 @@ namespace M09UF2EA2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] cadenes = new[]{
+                "Una vegada hi havia un gat...",
+                "En un lugar de la Mancha...",
+                "Once upon a time in the west..."
+            };
+            List<Thread> threads = new List<Thread>();
+            int currentThread = 0;
+            foreach (var cadena in cadenes)
+            {
+                int current = currentThread++;
+                Thread thread = new Thread(() =>
+                {
+                    if (current != 0)
+                        threads[current - 1].Join();
+                    WriteSentence(cadena, 1);
+                });
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            WaitForThreads(threads);
+        }
+
+        static void WriteSentence(string cadena, int timeBetweenWords)
+        {
+            string[] cadenaSplit = cadena.Split(" ");
+            for (int i = 0; i < cadenaSplit.Length; i++)
+            {
+                if (i != 0)
+                {
+                    Console.Write(" ");
+                    Thread.Sleep(timeBetweenWords * 1000);
+                }
+                Console.Write(cadenaSplit[i]);
+            }
+            Console.Write("\n");
+        }
+
+        static void WaitForThreads(List<Thread> threads)
+        {
+            bool haAcabat = false;
+            Thread finishingThread = new Thread(() =>
+            {
+                foreach (var thread in threads)
+                    thread.Join();
+                haAcabat = true;
+            });
+            finishingThread.Start();
+
+            while (!haAcabat) ;
         }
     }
 }
